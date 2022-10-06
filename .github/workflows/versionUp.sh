d35cca4cd93b3cb322c3a9209fe1cae666a7f3cb
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2022-10-06 16:49:23 -0400

    Action script should be working

--- .github/workflows/versionUp.sh
+++ .github/workflows/versionUp.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+TODAY=$( date -u '+%Y-%m-%d' )
+MESSAGE="chore: regenerate $FILE_TO_COMMIT for $TODAY"
+SHA=$( git rev-parse "$DESTINATION_BRANCH":"$FILE_TO_COMMIT" )
+CONTENT=$( base64 -i "$FILE_TO_COMMIT" )
 if [[ -n "$(git status --porcelain)" ]]; then
-  gh api --method PUT /repos/:owner/:repo/contents/$FILE_TO_COMMIT \
+  gh api --method PUT /repos/:owner/:repo/contents/"$FILE_TO_COMMIT" \
           --field message="$MESSAGE" \
           --field content="$CONTENT" \
           --field encoding="base64" \
