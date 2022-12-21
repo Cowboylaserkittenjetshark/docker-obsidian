99e445d438a071b054b39d8c82a0905ac4923a1d
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2022-12-20 21:48:19 -0500

    Don't use exit codes to avoid job fail

--- .github/workflows/commitChange.sh
+++ .github/workflows/commitChange.sh
@@ -10,7 +10,7 @@ if [[ -n "$(git status --porcelain)" ]]; then
           --field encoding="base64" \
           --field branch="$DESTINATION_BRANCH" \
           --field sha="$SHA"
-  exit 0
+  echo "Commited"
 else
-  exit 255
+  echo "Nothing to commit"
 fi
